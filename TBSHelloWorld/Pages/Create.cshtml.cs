c10141a1613499c64a8aa91ed579bb4abc5ecbc6
TBSService <TBSService@gmail.com>
2025-03-07 11:11:40 +0800

    預設

---
+++ TBSHelloWorld/Pages/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TBSHelloWorld.Pages
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- TBSHelloWorld/wwwroot/js/site.js
+++ TBSHelloWorld/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var header = document.getElementById("header");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+window.onscroll = function () { myFunction() };
